a5bb6a2eaa9dda9f75a6294ce206df97781d1cc8
masalgaro <masalgaro@eafit.edu.co>
2026-05-09 03:02:03 -0500

    Inicio base

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const app = express()
 const os = require("os")
 const port = 80
 
+// Lectura del JSON 
+fs.readFile('pokeamigos-db.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+
+  const pokeamigos_db = JSON.parse(data);
+
+  console.log(pokeamigos_db);
+});
+
+// Middleware para conseguir un amigo aleatorio
+app.use((req, res, next) => {
+  const num_amigo = Math.floor(Math.random() * Object.keys(pokeamigos_db.shareInfo[i]).length);
+  next();
+});
+
+app.get('/', (req, res) => {
+});
